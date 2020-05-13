25d921365f773d4b74e3e4a5d2114382c426ce54
Nathan Moritz <nateim3@gmail.com>
2020-05-13 12:47:27 -0400

    Added Profile struct and url

--- Utils/getUtils.go
+++ Utils/getUtils.go
@@ -8,7 +8,7 @@ import (
 	"net/url"
 )
 
-type Profile struct {
+type Quote struct {
 	Symbol string `json:"symbol"`
 	Name string `json:"name"`
 	Price float64 `json:"price"`
@@ -31,16 +31,40 @@ type Profile struct {
 	SharesOutstanding int64 `json:"sharesOutstanding"`
 }
 
+type Profile struct {
+	Symbol string `json:"symbol"`
+	Price float64 `json:"price"`
+	Beta float64 `json:"beta"`
+	VolAvg float64 `json:"volAvg"`
+	MktCap float64 `json:"mktCap"`
+	LastDiv float32 `json:"lastDiv"`
+	Range float64 `json:"range"`
+	Changes float32 `json:"Changes"`
+	ChangesPercentage float32 `json:"changesPercentage"`
+	CompanyName string `json:"companyName"`
+	Exchange string `json:"exchange"`
+	Industry string `json:"industry"`
+	Website string `json:"website"`
+	Description string `json:"description"`
+	Ceo string `json:"ceo"`
+	Sector string `json:"sector"`
+	Image string `json:"image"`
+}
 
-
-var baseUrl = url.URL{
+var quoteUrl = url.URL{
 	Scheme: "https",
 	Host: "financialmodelingprep.com",
-	Path: "/api/v3/quote/AAPL,FB",
+	Path: "/api/v3/quote/",
+}
+
+var profileUrl = url.URL{
+	Scheme:     "https",
+	Host:       "financialmodelingprep.com",
+	Path:       "/api/v3/company/profile",
 }
 
 func GetProfile() {
-	response, err := http.Get(baseUrl.String())
+	response, err := http.Get(quoteUrl.String())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,7 +74,7 @@ func GetProfile() {
 		log.Fatal(err)
 	}
 
-	var data []Profile
+	var data []Quote
 
 	jsonErr := json.NewDecoder(response.Body).Decode(&data)
 	if jsonErr != nil {
