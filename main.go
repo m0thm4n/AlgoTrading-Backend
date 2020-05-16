665cb734f2dca8ade34dae2d86ec4ea98fde48b7
Nathan Moritz <nateim3@gmail.com>
2020-05-16 11:35:47 -0400

    Added initial API code

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"AlgoTrading/Database"
+	"AlgoTrading/API"
 	"AlgoTrading/Utils"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	quote := Utils.GetProfile()
+	quote := Utils.GetQuote()
+	fmt.Println(quote)
 
-	Database.DbConnect("localhost")
+	// Database.DbConnect("localhost")
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", API.HomeLink)
+	router.HandleFunc("/event", API.CreateQuote)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
-
