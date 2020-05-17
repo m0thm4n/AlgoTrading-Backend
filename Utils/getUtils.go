c4ebdef4abdd631fca70e75d209d3d286dac7c91
Nathan Moritz <nateim3@gmail.com>
2020-05-17 15:20:53 -0400

    Removed react, using Plotly Python library instead. Finished Profile endpoint

--- Utils/getUtils.go
+++ Utils/getUtils.go
@@ -2,6 +2,7 @@ package Utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -30,24 +31,26 @@ type Quote struct {
 	SharesOutstanding int64   `json:"sharesOutstanding"`
 }
 
-type Profile struct {
+type WholeProfile struct {
 	Symbol            string  `json:"symbol"`
-	Price             float64 `json:"price"`
-	Beta              float64 `json:"beta"`
-	VolAvg            float64 `json:"volAvg"`
-	MktCap            float64 `json:"mktCap"`
-	LastDiv           float32 `json:"lastDiv"`
-	Range             float64 `json:"range"`
-	Changes           float32 `json:"Changes"`
-	ChangesPercentage float32 `json:"changesPercentage"`
-	CompanyName       string  `json:"companyName"`
-	Exchange          string  `json:"exchange"`
-	Industry          string  `json:"industry"`
-	Website           string  `json:"website"`
-	Description       string  `json:"description"`
-	Ceo               string  `json:"ceo"`
-	Sector            string  `json:"sector"`
-	Image             string  `json:"image"`
+	Profile struct {
+		Price             float64 `json:"price"`
+		Beta              string `json:"beta"`
+		VolAvg            string `json:"volAvg"`
+		MktCap            string `json:"mktCap"`
+		LastDiv           string `json:"lastDiv"`
+		Range             string `json:"range"`
+		Changes           float64 `json:"Changes"`
+		ChangesPercentage string `json:"changesPercentage"`
+		CompanyName       string  `json:"companyName"`
+		Exchange          string  `json:"exchange"`
+		Industry          string  `json:"industry"`
+		Website           string  `json:"website"`
+		Description       string  `json:"description"`
+		Ceo               string  `json:"ceo"`
+		Sector            string  `json:"sector"`
+		Image             string  `json:"image"`
+	} `json:"profile"`
 }
 
 var quoteUrl = url.URL{
@@ -81,4 +84,28 @@ func GetQuote(symbol string) []Quote {
 	}
 
 	return data
+}
+
+func GetProfile(symbol string) {
+	response, err := http.Get(profileUrl.String() + symbol)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data WholeProfile
+
+	// err = json.NewDecoder(response.Body).Decode(&data)
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(data)
+
+	// return data
 }
--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"AlgoTrading/API"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"AlgoTrading/Utils"
 )
 
 func main() {
 	// quote := Utils.GetQuote()
 	// fmt.Println(quote)
 
+	Utils.GetProfile("AAPL")
+
 	// session := Database.DbConnect("localhost:8080/")
 	// Database.CreateTable(session)
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", API.HomeLink)
-	router.HandleFunc("/api/quote/{symbol}", API.CreateQuote).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// router := mux.NewRouter().StrictSlash(true)
+	// router.HandleFunc("/", API.HomeLink)
+	// router.HandleFunc("/api/quote/{symbol}", API.CreateQuote).Methods("GET")
+	// log.Fatal(http.ListenAndServe(":8080", router))
+
+	// Server.Routes()
 }
