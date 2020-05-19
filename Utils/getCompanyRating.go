9b35799999cef8f78a6b679c8464e7f2c80fd802
Nathan Moritz <nateim3@gmail.com>
2020-05-19 02:01:26 -0400

    Added Company Rating, Discounted Cash Flow, and Historical Discounted Cash Flow endpoint

--- Utils/getCompanyRating.go
+++ Utils/getCompanyRating.go
@@ -1,5 +1,12 @@
 package Utils
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+)
+
 type CompanyRating struct {
 	Symbol	string	`json:"symbol"`
 	Rating	[]struct {
@@ -35,3 +42,33 @@ type CompanyRating struct {
 	} `json:"ratingDetails"`
 }
 
+var companyRatingUrl = url.URL {
+	Scheme: "https",
+	Host:   "financialmodelingprep.com",
+	Path:   "/api/v3/company/rating/",
+}
+
+// Gets Company Rating from financialmodelingprep.com
+func GetCompanyRating(symbol string) CompanyRating {
+	response, err := http.Get(companyRatingUrl.String() + symbol)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data CompanyRating
+
+	// err = json.NewDecoder(response.Body).Decode(&data)
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+
+	// return data
+}
---
+++ Utils/getDiscountedCashFlow.go
@@ -0,0 +1,44 @@
+package Utils
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type DiscountedCashFlow struct {
+	Symbol		string	`json:"symbol"`
+	Date		string	`json:"date"`
+	DFC			float64	`json:"dfc"`
+	StockPrice	float64	`json:"Stock Price"`
+}
+
+var discountedCashFlow = url.URL {
+	Scheme: "https",
+	Host:   "financialmodelingprep.com",
+	Path:   "/api/v3/company/discounted-cash-flow/",
+}
+
+
+// Series of functions for getting Discounted Cash Flow and Historical Discounted Cash Flow from financialmodelingprep.com
+func GetDiscountedCashFlow(symbol string) DiscountedCashFlow {
+	response, err := http.Get(discountedCashFlow.String() + symbol)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data DiscountedCashFlow
+
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
