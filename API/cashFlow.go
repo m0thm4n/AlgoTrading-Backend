0f9efc7eb311085befcc3c96c92aac7c53a79b1b
Nathan Moritz <nateim3@gmail.com>
2020-05-18 20:07:10 -0400

    Added Cash Flow Statement endpoint

---
+++ API/cashFlow.go
@@ -0,0 +1,71 @@
+package API
+
+import (
+	"AlgoTrading/Utils"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func GetCashFlowStatement(w http.ResponseWriter, r *http.Request) {
+	symbol := mux.Vars(r)["symbol"]
+
+	cashFlowStatement := Utils.GetCashFlowStatement(symbol)
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(requestBody, &cashFlowStatement)
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(cashFlowStatement)
+}
+
+func GetQuarterlyCashFlowStatement(w http.ResponseWriter, r *http.Request) {
+	symbol := mux.Vars(r)["symbol"]
+
+	cashFlowStatement := Utils.GetQuarterlyCashFlowStatement(symbol)
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(requestBody, &cashFlowStatement)
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(cashFlowStatement)
+}
+
+func GetCsvCashFlowStatement(w http.ResponseWriter, r *http.Request) {
+	symbol := mux.Vars(r)["symbol"]
+
+	cashFlowStatement := Utils.GetQuarterlyCashFlowStatement(symbol)
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(requestBody, &cashFlowStatement)
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(cashFlowStatement)
+}
+
+func GetQuarterlyCsvCashFlowStatement(w http.ResponseWriter, r *http.Request) {
+	symbol := mux.Vars(r)["symbol"]
+
+	cashFlowStatement := Utils.GetQuarterlyCashFlowStatement(symbol)
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(requestBody, &cashFlowStatement)
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(cashFlowStatement)
+}
